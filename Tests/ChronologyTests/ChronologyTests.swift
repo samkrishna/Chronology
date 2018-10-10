925745757377ff6f0e920db20dabd19b2020035c
Sam Krishna <sam.krishna@gmail.com>
2018-10-10 10:44:59 -0700

    Added comparison test

--- Tests/ChronologyTests/ChronologyTests.swift
+++ Tests/ChronologyTests/ChronologyTests.swift
@@ -3,13 +3,19 @@ import XCTest
 
 class ChronologyTests: XCTestCase {
     func testExample() {
-        
         let c = Clock.system
         let today = c.thisNanosecond()
         let v = today + .nanoseconds(2)
         XCTAssertNotNil(v)
     }
 
+    func testComparisons() {
+        let c = Clock.system
+        let nanoNow = c.thisNanosecond()
+        let nanoFuture = nanoNow + .nanoseconds(10)
+//        XCTAssertThrowsError(nanoFuture > nanoNow)
+        XCTAssertNotNil(nanoFuture)
+    }
 
     static var allTests = [
         ("testExample", testExample),
